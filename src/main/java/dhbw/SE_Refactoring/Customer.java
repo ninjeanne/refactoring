18800e1ee4ce4278fe3ed4256b6d2bf970a5d9e9
Jeanne Helm <jeanne.helm@diva-e.com>
2020-05-14 10:13:41 +0200

    rename variable each to current rental. Also replace enumeration with for each to make it more readable and remove unneccessary variable enum_rentals.
    Fowler: Rename Variable
    https://refactoring.com/catalog/renameVariable.html

--- src/main/java/dhbw/SE_Refactoring/Customer.java
+++ src/main/java/dhbw/SE_Refactoring/Customer.java
@@ -1,13 +1,10 @@
 package dhbw.SE_Refactoring;
 
-import java.util.Enumeration;
 import java.util.Vector;
-
-import static dhbw.SE_Refactoring.PriceCode.NEW_RELEASE;
 
 class Customer {
     private String name;
-    private Vector rentals = new Vector();
+    private Vector<Rental> rentals = new Vector();
 
     public Customer(String name) {
         this.name = name;
@@ -24,44 +21,45 @@ class Customer {
     public String statement() {
         TotalAmount totalAmount = new TotalAmount();
         RenterPoints renterPoints = new RenterPoints();
-        Enumeration enum_rentals = rentals.elements();
+
         String result = printCustomerDetails();
         result += printHeader();
 
-        while (enum_rentals.hasMoreElements()) {
+        for (Rental currentRental : rentals) {
             double thisAmount;
-            Rental each = (Rental) enum_rentals.nextElement();
-
-            thisAmount = amountFor(each);
+            thisAmount = amountFor(currentRental);
             renterPoints.increase();
-            renterPoints.increaseBonus(each);
-            result += printDetails(each, thisAmount);
+            renterPoints.increaseBonus(currentRental);
+            result += printDetails(currentRental, thisAmount);
             totalAmount.increase(thisAmount);
         }
+
         result += printFooter(totalAmount, renterPoints);
         return result;
     }
 
-    private String printCustomerDetails(){
+    private String printCustomerDetails() {
         return "Rental Record for " + this.getName() + "\n";
     }
 
-    private String printHeader(){
+    private String printHeader() {
         return "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
     }
 
     /**
      * add footer lines
+     *
      * @param totalAmount the calculated total amount of the renting of the customer
      * @param renterPoints the renter points for the customer
      * @return the footer lines
      */
-    private String printFooter(TotalAmount totalAmount, RenterPoints renterPoints){
+    private String printFooter(TotalAmount totalAmount, RenterPoints renterPoints) {
         return totalAmount.toString() + renterPoints.toString();
     }
 
     /**
      * show figures for this rental
+     *
      * @param rental current rental
      * @param amount price amount of current rental
      * @return Details of the rental including the movie title, the days rented and the amount
@@ -72,6 +70,7 @@ class Customer {
 
     /**
      * determine amounts for current rental
+     *
      * @param each current rental
      * @return price of current rental
      */
